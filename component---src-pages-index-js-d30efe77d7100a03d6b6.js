6178fd90f40c6a54ccc3b557857010df8f143eef
Travis CI User <travis@example.org>
2020-08-11 11:20:31 +0000

    Updates

---
+++ component---src-pages-index-js-d30efe77d7100a03d6b6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),c=t.n(o);e.default=function(){return c.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d30efe77d7100a03d6b6.js.map
